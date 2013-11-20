cd8a0e3bd630eb8ae6fcff76fc31d47efa9cc28d
Tom Dyer <tdyer1@gmail.com>
2013-11-20 08:58:08 -0500

    start of fixing playlist creation

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,14 @@
 class Playlist < ActiveRecord::Base
+
+  STATES = [PRIVATE = 'private', PUBLIC = 'public']
   belongs_to :user, touch: true
   has_and_belongs_to_many :songs
+
+  validates :states, inclusion: { in: STATES }
+
+  def self.states_for_select_tag
+    s = ""
+    Playlist::STATES.each_with_index { |e, i| s <<  "<option value=#{i}>#{e}</option>"}
+    s.html_safe
+  end
 end
